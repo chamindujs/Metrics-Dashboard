0708385d37bf27d92e1323213510826d773f9704
Nicholas Synovic <Nicholas.Synovic@gmail.com>
2020-09-22 22:36:17 -0500

    Remove comments

--- Commits/Code/TokenHandler.py
+++ Commits/Code/TokenHandler.py
@@ -3,20 +3,20 @@ import os
 
 class TokenHandler:
     def __init__(self):
-        if os.path.isfile("tokens.txt"):  # Boolean
+        if os.path.isfile("tokens.txt"):
             pass
         else:
             open(
                 "tokens.txt", "w"
-            ).close()  # Gaurentees creation of tokens.txt if it is not created
+            ).close()
 
     def write(self, token: str) -> None:
-        with open("tokens.txt", "a+") as tokensFile:  # Opens file in appending mode
-            tokensFile.write(token + "\n")
+        with open("tokens.txt", "a+") as tokensFile:
+             tokensFile.write(token + "\n")
             tokensFile.close()
 
     def read(self) -> list:
-        with open("tokens.txt", "r") as tokensFile:  # Opens the file in read mode
+        with open("tokens.txt", "r") as tokensFile:
             fileLines = tokensFile.readlines()
             tokensFile.close()
         return [line.replace("\n", "") for line in fileLines]
