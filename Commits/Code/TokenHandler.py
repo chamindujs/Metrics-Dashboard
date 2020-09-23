1d9236bfb7040e820f7aff2e9de99afe33a2f47b
Nicholas Synovic <Nicholas.Synovic@gmail.com>
2020-09-22 21:58:17 -0500

    Ran black formatter

--- Commits/Code/TokenHandler.py
+++ Commits/Code/TokenHandler.py
@@ -2,36 +2,35 @@ import os
 
 
 class TokenHandler:
-
     def __init__(self):
-        self.filename = "tokens.txt"
-
-        if os.path.isfile(self.filename):   # Boolean
+        if os.path.isfile("tokens.txt"):  # Boolean
             pass
         else:
-            open("tokens.txt", "w").close()  # Gaurentees creation of tokens.txt if it is not created
-    
-    def write(self, token:str, mode:str="a+")  ->  None:
-        keysfile = open(self.filename, mode)    # Opens file in appending mode
+            open(
+                "tokens.txt", "w"
+            ).close()  # Gaurentees creation of tokens.txt if it is not created
+
+    def write(self, token: str, mode: str = "a+") -> None:
+        keysfile = open("tokens.txt", mode)  # Opens file in appending mode
         keysfile.writelines(token + "\n")
         keysfile.close()
 
-    def writelines(self, data:list=None, mode:str="a+")    ->  None:
-        keysfile = open(self.filename, mode)
+    def writelines(self, data: list = None, mode: str = "a+") -> None:
+        keysfile = open("tokens.txt", mode)
         for item in data:
             keysfile.write(item + "\n")
         keysfile.close()
 
-    def read(self)    ->  list:
-        keysfile = open(self.filename, "r") # Opens the file in read mode
+    def read(self) -> list:
+        keysfile = open("tokens.txt", "r")  # Opens the file in read mode
         foo = keysfile.readlines()
         keysfile.close()
         return [x.replace("\n", "") for x in foo]
 
-    def deleteValue(self, value:str=None)   ->  None:
-        '''
-Deletes all instances of a value in the file.
-        '''
+    def deleteValue(self, value: str = None) -> None:
+        """
+        Deletes all instances of a value in the file.
+        """
         data = self.read()
         for x in range(len(data)):
             try:
