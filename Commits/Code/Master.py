279a501750dff1163836315a7cc1248f12c0f701
Nicholas Synovic <Nicholas.Synovic@gmail.com>
2020-09-22 21:55:00 -0500

    Ran isort

--- Commits/Code/Master.py
+++ Commits/Code/Master.py
@@ -1,7 +1,10 @@
 from datetime import datetime, timedelta
+from sqlite3 import Connection, Cursor
+
 from githubAPI import GitHubAPI
-from sqlite3 import Cursor, Connection
+
 import Commits
+
 
 class Logic:
     '''
--- Commits/Code/TokenHandler.py
+++ Commits/Code/TokenHandler.py
@@ -1,5 +1,6 @@
 import os
 
+
 class TokenHandler:
 
     def __init__(self):
