933353eaf5fc10fa107153cca4866d03698b2994
Nicholas Synovic <Nicholas.Synovic@gmail.com>
2020-09-22 22:08:11 -0500

    Make all TokenHandler.py files act the same

--- Lines_Of_Code_Num_Of_Chars/Code/TokenHandler.py
+++ Lines_Of_Code_Num_Of_Chars/Code/TokenHandler.py
@@ -1,40 +1,22 @@
 import os
 
+
 class TokenHandler:
-
     def __init__(self):
-        self.filename = "tokens.txt"
-
-        if os.path.isfile(self.filename):   # Boolean
+        if os.path.isfile("tokens.txt"):  # Boolean
             pass
         else:
-            open("tokens.txt", "w").close()  # Gaurentees creation of tokens.txt if it is not created
-    
-    def write(self, token:str, mode:str="a+")  ->  None:
-        keysfile = open(self.filename, mode)    # Opens file in appending mode
-        keysfile.writelines(token + "\n")
-        keysfile.close()
+            open(
+                "tokens.txt", "w"
+            ).close()  # Gaurentees creation of tokens.txt if it is not created
 
-    def writelines(self, data:list=None, mode:str="a+")    ->  None:
-        keysfile = open(self.filename, mode)
-        for item in data:
-            keysfile.write(item + "\n")
-        keysfile.close()
+    def write(self, token: str) -> None:
+        with open("tokens.txt", "a+") as tokensFile:  # Opens file in appending mode
+            tokensFile.write(token + "\n")
+            tokensFile.close()
 
-    def read(self)    ->  list:
-        keysfile = open(self.filename, "r") # Opens the file in read mode
-        foo = keysfile.readlines()
-        keysfile.close()
-        return [x.replace("\n", "") for x in foo]
-
-    def deleteValue(self, value:str=None)   ->  None:
-        '''
-Deletes all instances of a value in the file.
-        '''
-        data = self.read()
-        for x in range(len(data)):
-            try:
-                data.remove(value)
-            except ValueError:
-                break
-        self.writelines(data=data, mode="w")
+    def read(self) -> list:
+        with open("tokens.txt", "r") as tokensFile:  # Opens the file in read mode
+            fileLines = tokensFile.readlines()
+            tokensFile.close()
+        return [line.replace("\n", "") for line in fileLines]
