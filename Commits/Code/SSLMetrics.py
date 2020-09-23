2ca6482749ac6739aed41f7e3cb5d4d701ecd758
Nicholas Synovic <Nicholas.Synovic@gmail.com>
2020-09-23 12:22:00 -0500

    Remove comments and docstrings

--- Commits/Code/SSLMetrics.py
+++ Commits/Code/SSLMetrics.py
@@ -7,37 +7,18 @@ from TokenHandler import TokenHandler
 
 
 class SSLMetrics:
-    """
-    This is what should be called to actually run the SSL Metrics tool.\n
-    Call this tool in the command line as: python SSLMetrics.py {GitHub URL} {Optional Token}
-    """
-
     def __init__(self) -> None:
-        """
-        Initializes the program and sets class variables that are going to be used as the initial values across the program.\n
-        Required command line arguements:\n
-        GitHub URL (https://github.com/{Username}/{Repository})\n
-        Optional command line arguements:\n
-        GitHub Personal Access Token
-        """
-        self.args = sys.argv[1:]  # All of the command line args excluding the filename
+        self.args = sys.argv[1:]
         self.githubURL = None
         self.githubUser = None
         self.githubRepo = None
         self.githubToken = None
-        self.githubTokenList = None  # This is pulled from keys.txt
-        self.dbCursor = None  # Database specific variable
-        self.dbConnection = None  # Database specific variables
-        self.th = (
-            TokenHandler()
-        )  # Class instance to write and read tokens to tokens.txt
+        self.githubTokenList = None
+        self.dbCursor = None
+        self.dbConnection = None
+        self.th = TokenHandler()
 
     def parseArgs(self) -> None:
-        """
-        This is a REQUIRED method.\n
-        Logic to parse the list of command line arguements to make sure that they meet program requirements.\n
-        Will also generate the keys.txt file, get data from it, and potentially write data to it as well.
-        """
 
         if len(self.args) > 2:
             sys.exit("Too Many Args")
@@ -49,7 +30,7 @@ class SSLMetrics:
             self.githubToken = self.args[1]
             self.th.write(token=self.githubToken)
             self.githubTokenList = self.th.read()
-        except IndexError:  # There was no token as an arg
+        except IndexError:
             self.githubTokenList = self.th.read()
             try:
                 self.githubToken = self.githubTokenList[0]
@@ -57,12 +38,6 @@ class SSLMetrics:
                 pass
 
     def stripURL(self) -> None:
-        """
-        This is a REQUIRED method.
-        Logic to parse the URL arguement to make sure it contains both a username and a repository.\n
-        Logic will error out if an invalid URL is the arguement.\n
-        Further checks are made on the URL in the GitHubAPI.py file. It is possible for a URL to pass these tests here, however the program will error out if it fails other tests down the road.
-        """
 
         if self.githubURL.find("github.com/") == -1:
             sys.exit("Invalid URL Arg")
@@ -76,13 +51,9 @@ class SSLMetrics:
         self.githubRepo = foo[-1]
 
     def launch(self) -> None:
-        """
-        This is a REQUIRED method.\n
-        Logic to actually begin the analysis.
-        """
         self.dbCursor, self.dbConnection = sqlite_database.open_connection(
             self.githubRepo
-        )  # Unsure of what this code does due to lack of knowledge on how the database works
+        )
         Master.Logic(
             username=self.githubUser,
             repository=self.githubRepo,
