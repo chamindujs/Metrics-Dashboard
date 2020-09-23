f5525ac4e2c174821bab012829a269a624b4e0b6
Nicholas Synovic <Nicholas.Synovic@gmail.com>
2020-09-23 08:45:32 -0500

    Create a class to handle SQLLite DB Creation and Connection

---
+++ Commits/Code/__init__.py
@@ -0,0 +1,6 @@
+from . import Commits
+from . import githubAPI
+from . import Master
+from . import sqlite_database
+from . import SSLMetrics
+from . import TokenHandler
--- Commits/Code/sqlite_database.py
+++ Commits/Code/sqlite_database.py
@@ -1,28 +1,57 @@
 import os
 import sqlite3
+from time import time
 
 
-def open_connection(repo_name):
-    """
-    This is some SQL code that creates the tables and columns in a database named after the repository its data is holding.
-    """
-    try:
-        connection = sqlite3.connect("/metrics/" + str(repo_name) + ".db")
-    except sqlite3.OperationalError:
-        connection = sqlite3.connect("/metrics/" + str(repo_name) + ".db")
+class DBConnection:
+    def __init__(self, directory: str = "/metrics", ghRepo: str = "temporary"):
 
-    cursor = connection.cursor()
+        count = 0
+        fileExists = True
+        directoryExists = False
 
-    # Create table - COMMITS
-    cursor.execute(
-        "CREATE TABLE IF NOT EXISTS COMMITS (author VARCHAR(3000), author_date VARCHAR(3000), committer VARCHAR(3000), committer_date VARCHAR(3000), commits_url VARCHAR(3000), message VARCHAR(30000), comment_count VARCHAR(3000), comments_url VARCHAR(3000));"
-    )
+        self.filePath = None
 
-    # Create table - MASTER
-    cursor.execute(
-        "CREATE TABLE IF NOT EXISTS MASTER(date DATE, commits INT(3000), issues INT(3000), defect_density INT(3000), issue_spoilage_avg INT(3000), issue_spoilage_max INT(3000), issue_spoilage_min INT(3000), lines_of_code INT(300), num_of_chars INT(300), PRIMARY KEY (date));"
-    )
+        dbFilePath = (
+            lambda path, num: path
+            + r"/"
+            + ghRepo
+            + "_"
+            + str(int(time()))
+            + "_"
+            + str(num)
+            + ".db"
+        )
 
-    connection.commit()
+        while directory is False:
+            if os.path.isdir(directory):
+                directoryExists = True
+            else:
+                os.mkdir(directory)
 
-    return cursor, connection
+        while fileExists is True:
+            if os.path.isfile(dbFilePath(directory, count)):
+                count += 1
+            else:
+                self.filePath = dbFilePath(directory, count)
+                fileExists = False
+
+    def open_connection(repo_name):
+        try:
+            connection = sqlite3.connect("/metrics/" + str(repo_name) + ".db")
+        except sqlite3.OperationalError:
+            connection = sqlite3.connect("/metrics/" + str(repo_name) + ".db")
+
+        cursor = connection.cursor()
+
+        cursor.execute(
+            "CREATE TABLE IF NOT EXISTS COMMITS (author VARCHAR(3000), author_date VARCHAR(3000), committer VARCHAR(3000), committer_date VARCHAR(3000), commits_url VARCHAR(3000), message VARCHAR(30000), comment_count VARCHAR(3000), comments_url VARCHAR(3000));"
+        )
+
+        cursor.execute(
+            "CREATE TABLE IF NOT EXISTS MASTER(date DATE, commits INT(3000), issues INT(3000), defect_density INT(3000), issue_spoilage_avg INT(3000), issue_spoilage_max INT(3000), issue_spoilage_min INT(3000), lines_of_code INT(300), num_of_chars INT(300), PRIMARY KEY (date));"
+        )
+
+        connection.commit()
+
+        return cursor, connection
